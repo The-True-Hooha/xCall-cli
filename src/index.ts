a8bb3266697e38d41ce13ed626e6665b9395470f
Davidhero <owogogahhero@outlook.com>
2023-11-29 13:59:21 +0100

    init: setup

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import * as figlet from 'figlet';
+import { Command } from 'commander';
+
+console.log(figlet.textSync('hello world'));
+
+const cli = new Command();
+cli
+  .version('1.0')
+  .description(
+    'a cross-chain application compatibility check with the xCall network',
+  )
+  .option('-l, --list [value]', 'list the available supported chains')
+  .option(
+    '-v, --validate <value>, validates a smart contract against or more supported chains',
+  )
+  .option('-c, --chain <value>, specify chains to validate against')
+  .option(
+    '-n, --network <value>, specify the network for which the contract is intended',
+  )
+  .option(
+    '-o, --output <value>, define the output file format for the validation result(e.g., JSON, YAML, txt)',
+  )
+  .option(
+    '-v, --verbose <value>, enable to provide more information during validation',
+  )
+  .option(
+    '-i, --ignore <value>, skips warnings during validation, only displaying critical errors.',
+  )
+  .option(
+    '-h, --help <value>, displays help information about the CLI and its available commands/options.',
+  )
+  .parse(process.argv);
+
+const options = cli.opts();
+if (!process.argv.slice(2).length) {
+  cli.outputHelp();
+}
